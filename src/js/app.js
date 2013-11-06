e66071ce4e4255dfaa378aa608f851763c974653
Joshua T Kalis <kalisjoshua@gmail.com>
2013-11-05 21:57:28 -0500

    shrink header font size on scroll

--- src/js/app.js
+++ src/js/app.js
@@ -2,46 +2,36 @@
 
 $.fn.ready(function () {
   var $articles = $('article')
+    , $body = $('body')
     , $tagsFilter = $('.tags--filter')
 
     , hash = window.location.hash
     , tags = [];
 
-  if (hash && /=/.test(hash)) {
-    tags = hash
-      .match(/^#filter=(.*)?$/)
-      .pop()
-      .split(',');
+  function disqusInit (disqus_shortname) {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
   }
-
-  $articles
-    .each(function tagsTextToLinks (indx) {
-      var article,
-          blockTags,
-          tagsBlock;
-
-      article = $(this);
-
-      tagsBlock = article
-        .find('.tags');
-
-      blockTags = tagsBlock
-        .text()
-        .split(', ');
-
-      tagsBlock
-        .html(tagLinks(blockTags));
-    });
-
-  $(document)
-    .on('click', '.addTag', tagClickHandler)
-    .on('click', '.drop--handle', dropClickHandler);
 
   function dropClickHandler () {
     $(this)
       .closest('.drop')
       .find('.drop--content')
       .slideToggle();
+  }
+
+  function highlight(js) {
+    return js
+      .replace(/([\[\]\{\}\(\)\.,;])/g, '<span class="punctuation">$1</span>')
+      .replace(/(^|\s)(\s*\/\/[^$]*?)(?=\n|$)/g, '$1<span class="comment">$2</span>')
+      .replace(/(\/\*[.\D]*?\*\/)/g, '<span class="comment">$1</span>')
+      .replace(/('.*?')/gm, '<span class="string">$1</span>')
+      .replace(/\s+(\/.+\/)([\.\s;])/g, '<span class="string">$1</span>$2')
+      .replace(/((?=[\-+])(?:[\-+]?\d+(?:\.\d+)?)|(?:\b\d+(?:\.\d+)?))/gm, '<span class="number">$1</span>')
+      .replace(/\bnew\s+(\w+)/gm, '<span class="keyword">new</span> <span class="init">$1</span>')
+      .replace(/\breturn\b/gm, '<span class="init">return</span>')
+      .replace(/\b(break|case|catch|continue|debugger|default|delete|do|else|finally|for|function|if|in|instanceof|switch|this|throw|try|typeof|var|void|while|with)\b/gm, '<span class="keyword">$1</span>');
   }
 
   function tagClickHandler (event) {
@@ -104,6 +94,50 @@ $.fn.ready(function () {
       .join(join || ', ');
   }
 
+  function tweetButton (d,s,id) {
+    var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+    if(!d.getElementById(id)){
+      js=d.createElement(s);
+      js.id=id;
+      js.src=p+'://platform.twitter.com/widgets.js';
+      fjs.parentNode.insertBefore(js,fjs);
+    }
+  }
+
+  if (hash && /=/.test(hash)) {
+    tags = hash
+      .match(/^#filter=(.*)?$/)
+      .pop()
+      .split(',');
+  }
+
+  $articles
+    .each(function tagsTextToLinks (indx) {
+      var article,
+          blockTags,
+          tagsBlock;
+
+      article = $(this);
+
+      tagsBlock = article
+        .find('.tags');
+
+      blockTags = tagsBlock
+        .text()
+        .split(', ');
+
+      tagsBlock
+        .html(tagLinks(blockTags));
+    });
+
+  $(document)
+    .on('click', '.addTag', tagClickHandler)
+    .on('click', '.drop--handle', dropClickHandler)
+    .on('scroll', function body_scroll_handler (event) {
+      $body
+        .toggleClass('scrolled', $body.scrollTop() > 20);
+    });
+
   tagFilter(tags);
 
   $('pre')
@@ -126,35 +160,3 @@ $.fn.ready(function () {
     disqusInit('joshuakalis');
   }
 });
-
-
-function highlight(js) {
-  return js
-    .replace(/([\[\]\{\}\(\)\.,;])/g, '<span class="punctuation">$1</span>')
-    .replace(/(^|\s)(\s*\/\/[^$]*?)(?=\n|$)/g, '$1<span class="comment">$2</span>')
-    .replace(/(\/\*[.\D]*?\*\/)/g, '<span class="comment">$1</span>')
-    .replace(/('.*?')/gm, '<span class="string">$1</span>')
-    .replace(/\s+(\/.+\/)([\.\s;])/g, '<span class="string">$1</span>$2')
-    .replace(/((?=[\-+])(?:[\-+]?\d+(?:\.\d+)?)|(?:\b\d+(?:\.\d+)?))/gm, '<span class="number">$1</span>')
-    .replace(/\bnew\s+(\w+)/gm, '<span class="keyword">new</span> <span class="init">$1</span>')
-    .replace(/\breturn\b/gm, '<span class="init">return</span>')
-    .replace(/\b(break|case|catch|continue|debugger|default|delete|do|else|finally|for|function|if|in|instanceof|switch|this|throw|try|typeof|var|void|while|with)\b/gm, '<span class="keyword">$1</span>');
-}
-
-
-function tweetButton (d,s,id) {
-  var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
-  if(!d.getElementById(id)){
-    js=d.createElement(s);
-    js.id=id;
-    js.src=p+'://platform.twitter.com/widgets.js';
-    fjs.parentNode.insertBefore(js,fjs);
-  }
-}
-
-
-function disqusInit (disqus_shortname) {
-  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-}
